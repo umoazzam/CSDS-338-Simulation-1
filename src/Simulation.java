ffbe3c2afdaf71a9ea549b4283a2b6d2b0c90571
Usman Moazzam <usmanmoazzam@hotmail.com>
2021-03-20 12:41:43 -0700

    finishing malloc

--- src/Simulation.java
+++ src/Simulation.java
@@ -22,13 +22,36 @@ public class Simulation {
    }
 
    public boolean malloc(String bName, int bSize) {
+      int blockIndex = 0;
       MemoryBlock b = new MemoryBlock();
 
-      // check if there already exists a block with the same name
-
+//      // check if there already exists a block with the same name
+//      if (checkReUse(bName) != -1) {
+//         System.out.println("Memory could not be allocated: " +
+//               bName + " block already being used");
+//      }
       // find a block to fit the current request
+      if ((blockIndex = checkFreeBlock(bSize)) == -1) {
+         System.out.println("Error: no memory available to allocate request");
+         return false;
+      }
 
-      // get the block
+      // get block and update fields
+      b = (MemoryBlock) MemoryList.elementAt(blockIndex);
+      int tempBlockSize = b.blockSize - bSize;
+      int tempEndAddr = b.endAddr;
+
+      b.blockStatus = true;
+      b.endAddr = b.startAddr + bSize;
+      b.blockSize = bSize;
+      b.blockName = bName;
+
+      if (tempBlockSize != 0) {
+         MemoryBlock newBlock = new MemoryBlock(
+               tempBlockSize,
+               b.startAddr + bSize,
+               tempEndAddr);
+      }
 
       return true;
    }
