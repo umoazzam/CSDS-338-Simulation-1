7e08516caff8b1086ae3092a7ac99b502ca7e20c
Usman Moazzam <usmanmoazzam@hotmail.com>
2021-03-20 10:52:48 -0700

    created memoryblock class

---
+++ src/MemoryBlock.java
@@ -0,0 +1,25 @@
+public class MemoryBlock {
+
+   public String blockName;
+   public boolean blockStatus;
+   public int blockSize;
+   public int startAddr;
+   public int endAddr;
+
+   public MemoryBlock(String blockName, boolean blockStatus, int blockSize,
+                      int startAddr, int endAddr) {
+      this.blockName = blockName;
+      this.blockStatus = blockStatus;
+      this.blockSize = blockSize;
+      this.startAddr = startAddr;
+      this.endAddr = endAddr;
+   }
+
+   public MemoryBlock(int blockSize, int startAddr, int endAddr) {
+      blockStatus = false;
+      this.blockSize = blockSize;
+      this.startAddr = startAddr;
+      this.endAddr = endAddr;
+   }
+
+}
